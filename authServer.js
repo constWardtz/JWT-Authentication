99101cc48efdfb161c86b6c2f0ad88a0dafe2288
constWardtz <edwardtutorials22@gmail.com>
2020-11-15 07:27:31 -0400

    Version 1.0.3

--- authServer.js
+++ authServer.js
@@ -6,12 +6,26 @@ const jwt = require('jsonwebtoken')
 /* Middleware */
 app.use(express.json())
 
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken })
+    })
+})
+
 app.post('/login', (req, res) => {
     const { username } = req.body
     const user = { name: username }
 
     const accessToken = generateAccessToken(user)
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
     res.json({ accessToken, refreshToken })
 
 })
